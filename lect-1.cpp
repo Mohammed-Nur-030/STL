b12add87de064eb069264440138f52e82feacb17
Mohammed-Nur-030 <126596779+Mohammed-Nur-030@users.noreply.github.com>
2023-09-09 14:57:51 +0530

    why namespace std?

--- lect-1.cpp
+++ lect-1.cpp
@@ -1,6 +1,16 @@
 #include<bits/stdc++.h>
-using namespace std;
+using namespace std;//used to declare a scope
+namespace raj{
+    int val=50;
+    int getVal(){
+        return val*10;
+    }
+}
 int main(){
+    double val=10.00;
+    cout<<val<<endl; //prints 10.00
+    cout<<raj::val<<endl;//prints 50
+    cout<<raj::getVal()<<endl; //prints 500
 
 
  return 0;
