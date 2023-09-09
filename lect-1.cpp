8a69eba1adbc5b345e8d9fa11f4d1fcad1495a4a
Mohammed-Nur-030 <126596779+Mohammed-Nur-030@users.noreply.github.com>
2023-09-09 15:07:10 +0530

    Pairs

--- lect-1.cpp
+++ lect-1.cpp
@@ -1,16 +1,31 @@
 #include<bits/stdc++.h>
-using namespace std;//used to declare a scope
-namespace raj{
-    int val=50;
-    int getVal(){
-        return val*10;
+using namespace std;
+
+
+//pairs
+void explianPairs(){
+    pair<int,int> p={1,3};
+    cout<<p.first<<" "<<p.second; 
+    cout<<endl;
+
+    pair<int, pair<int,int>> pt={1,{2,4}};
+    cout<<pt.first<<" "<<pt.second.first<<" "<<pt.second.second; 
+    cout<<endl;
+
+    pair<int,int> arr[]={{1,2},{3,4},{5,6}};
+    for (int i = 0; i < 3; i++)
+    {
+        cout<<arr[i].first<<" "<<arr[i].second;
+        cout<<endl;
     }
+    
+
+    return;
 }
 int main(){
-    double val=10.00;
-    cout<<val<<endl; //prints 10.00
-    cout<<raj::val<<endl;//prints 50
-    cout<<raj::getVal()<<endl; //prints 500
+    explianPairs();
+
+  
 
 
  return 0;
