a4261072b8054a0cd56d623ac1f5584f350d94ce
Mohammed-Nur-030 <126596779+Mohammed-Nur-030@users.noreply.github.com>
2023-09-09 15:31:05 +0530

    Vectors

--- lect-1.cpp
+++ lect-1.cpp
@@ -1,32 +1,75 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-
-//pairs
-void explianPairs(){
-    pair<int,int> p={1,3};
-    cout<<p.first<<" "<<p.second; 
-    cout<<endl;
-
-    pair<int, pair<int,int>> pt={1,{2,4}};
-    cout<<pt.first<<" "<<pt.second.first<<" "<<pt.second.second; 
-    cout<<endl;
-
-    pair<int,int> arr[]={{1,2},{3,4},{5,6}};
-    for (int i = 0; i < 3; i++)
+void printVector(vector<int> arr){
+    cout<<"Printing Array"<<endl;
+    for (auto i:arr)
     {
-        cout<<arr[i].first<<" "<<arr[i].second;
-        cout<<endl;
-    }
+      cout<<i<<" ";
+    }cout<<endl;
+    
+}
+void explainVector(){
+    vector<int> v;
+    v.push_back(1); //1
+    v.push_back(2);//1 2
+    v.emplace_back(3);//1 2 3
+
+    vector< pair <int,int>> vec;
+    vec.push_back({1,2});// {1,2}
+    vec.emplace_back(1,2);// {1,2}
+
+    vector <int> vec1(5,100); //5 positions will occupy 100 ie-> 100 100 100 100 100
+    vector <int> vec2(5); //5 positions of vector
+    /**************************************
+        * After declaring size of vector ,,you can modify it
+    **************************************/
+
+
+    vector <int> v1(5,20); //5 positions will occupy 100 ie-> 100 100 100 100 100
+    vector <int> v2(v1); //v2 is copy of v1
+
+    /**************************************
+            // Iterators
+    **************************************/
+
+    for (vector<int>::iterator it = v1.begin(); it != v1.end(); it++)
+    {
+        //  v1.begin()--is address not value
+       cout<<(*it)<<" ";
+    }cout<<endl;
+    
+    for (auto it = v1.begin(); it != v1.end(); it++)
+    {
+       cout<<(*it)<<" ";
+    }cout<<endl;
+
+    for (auto it:v1)
+    {
+       cout<<(it)<<" ";
+    }cout<<endl;
     
 
-    return;
+    vector <int> arr={10,20,30,40};
+
+    printVector(arr);// {10,20,30,40}
+    // v.erase(arr.begin()+1);
+    printVector(arr);//{10,30,40}
+
+    /**************************************
+        *Insertion
+    **************************************/
+   vector<int> vect(2,100);//{100 100}
+   vect.insert(v.begin(),300);// {300 100 100}
+   vect.insert(v.begin()+1,2,10);// {300 10 10 100 100}
+   vect.size();//5
+   vect.pop_back();//{300 10 10 100}
+
+
+    return ;
 }
 int main(){
-    explianPairs();
-
-  
+    explainVector();
 
 
- return 0;
+return 0;
 }
